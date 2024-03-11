b8a5d0bdb1c7d6194768f5af26550f806d496ea2
Oscar Maia <oscarfgmaia@gmail.com>
2024-03-10 21:38:11 -0300

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#2A2C2F',
+                secondary: '#A6AAB2',
+                tertiary: '#131416',
+            },
+        },
+        fontFamily: {
+            inter: ['Inter'],
+        },
+    },
+    plugins: [],
+}
